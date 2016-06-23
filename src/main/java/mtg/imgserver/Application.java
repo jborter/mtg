9fc3ba9d8e3425645878e46c235e3e1fe323a968
ailedt <deliatreichler@gmail.com>
2016-06-23 14:29:35 +0200

    introduce eureka

--- src/main/java/mtg/imgserver/Application.java
+++ src/main/java/mtg/imgserver/Application.java
@@ -1,17 +1,22 @@
 package mtg.imgserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by jbo on 20.06.2016.
  */
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication notificationMicroService = new SpringApplication(Application.class);
+        notificationMicroService.addListeners(new ApplicationPidFileWriter("notification-micro-service.pid"));
+        notificationMicroService.run(args);
     }
 }
